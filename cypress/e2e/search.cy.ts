bde6e17bcee15b8efc0798cbeaf6655c7df19059
Nazareth Blazquez <blazquezjimenezn@iesmartinrivero.org>
2022-06-16 12:27:01 +0200

    Solved T5

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -12,10 +12,19 @@ describe("Search elements", () => {
         });
     });
 
-    it("Search elements with no results", () => {
+    it("Search for elements with no results", () => {
         cy.search('qwerty');
         cy.fixture('searchResults').then((searchResults) => {
             cy.get(searchResults.alert). should('contain', 'No results were found for your search');
+        });
+    });
+
+    it.only("Search for elements with special code", () => {
+        cy.readFile('cypress/support/text/dataSearch.txt').then((text:string) => {
+            cy.search(text);
+            cy.fixture('searchResults').then((searchResults) => {
+                cy.get(searchResults.title). should('contain', text);
+            });
         });
     });
   });
